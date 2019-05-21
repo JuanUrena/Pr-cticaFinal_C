2b243c86759a581034fa19de11a059aabe148aab
JuanUrena <juanurenagarciagasco@gmail.com>
2019-05-21 19:48:46 +0200

    Controlado memoria y finaliza cuando llega al final de linea

--- read.c
+++ read.c
@@ -8,11 +8,20 @@ char* read_line(){
 	char *line=calloc(1,1);
 	char buffer[BUFFERSIZE];
 	
+	if (!line) {
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	
 	printf("Enter a message: \n");
 	
 	while(fgets(buffer, BUFFERSIZE , stdin) ){
   	
     	line = realloc(line, strlen(line)+1+strlen(buffer) );
+    	if (!line) {
+			fprintf(stderr, "lsh: allocation error\n");
+			exit(EXIT_FAILURE);
+		}
     	strcat(line, buffer);
     
     	if (buffer[strlen(buffer)-1]=='\n'){
@@ -31,12 +40,16 @@ int main() {
   char *text;
   
   
-  
-  while(1==1){
+  do{
 		text=read_line();
+		if (!strlen(text)){
+			free(text);
+			printf("\n-----FIN-----");
+  			return 0;
+		}
 		printf("\ntext:\n%s",text);
-		free(text);			    
-  }
+		free(text);
+  }while(1);
   
   return 0;
 }
