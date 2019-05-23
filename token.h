86fddf4238fc220188ef614a04230906a08edbd9
JuanUrena <juanurenagarciagasco@gmail.com>
2019-05-23 17:39:11 +0200

    Empezamos a separar la entrada y la salida del comando. Hemos creado un tipo de datos, pensando en recibir la linea entera y que esta funcion nos devuelva un tipo de datos, con la linea con los comandos y dos param con la entrada y la salida.

--- token.h
+++ token.h
@@ -1,6 +1,52 @@
+struct param_cmd{
+	char *comand;
+	char *in;
+	char *out;
+
+};
+
+
+
 
 struct list* tokenizar(char *line, char *cut);
+void get_in_out(char *line);
 
+
+void get_in_out(char *line){
+	char *in;
+	char *out;
+	struct param_cmd *param;
+	
+	param=(struct param_cmd *)malloc(sizeof(struct param_cmd));
+	
+	in=strchr(line,'<');
+	out=strchr(line, '>');
+	
+	if (strlen(in)>strlen(out)){
+		printf("Significa que in esta antes y por tanto in contiene a out");
+		out =strtok(out, ">");
+		param->out=(char *)malloc(sizeof(out)+1);
+		strcpy(param->out,out);
+		
+		in=strtok(in, "<");
+		in=strtok(in,">");
+		
+		param->in=(char *)malloc(sizeof(in)+1);
+		strcpy(param->in,in);
+	}else{
+		printf("significa que out es mas grande y es el que contine a in");
+		
+		in =strtok(in, "<");
+		param->in=(char *)malloc(sizeof(in)+1);
+		strcpy(param->in,in);
+		
+		out=strtok(out, ">");
+		out=strtok(out,"<");
+		
+		param->out=(char *)malloc(sizeof(out)+1);
+		strcpy(param->out,out);
+	};
+} 
 
 struct list* tokenizar(char *line, char *cut){
 	struct list *mylist;
